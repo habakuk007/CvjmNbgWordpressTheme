68da7b8b6789928b82bac941614601804afdf3cb
Oli Weber <almoeli@gmx.de>
2014-04-14 21:13:36 +0200

    Highlight Filter jetzt mit in der Navigationsleiste.

--- event-box.php
+++ event-box.php
@@ -24,48 +24,6 @@
       $event_show_filter = array( 'highlight' => 'no', 'noheadline' => 'no' );
     }
 	
-	// Filter out not query options we will fill in later
-	if (isset($event_add_query))
-	{
-	  $isHighlight = strpos($event_add_query, 'highlight=high');
-      $arg_array = wp_parse_args( $event_add_query );
-	  $newArgs = '';
-	  foreach ($arg_array as $key => $value) {
-	    if (strcmp($key, 'highlight') != 0) {
-	      if (strlen($newArgs) != 0)
-          {
-		    $newArgs .= "&";
-		  }
-		  $newArgs .= $key . "=" . $value;
-	    }
-	  }
-	} else {
-	  $isHighlight = false;
-	}
-		
-    if (array_key_exists('highlight', $event_show_filter) && strcmp($event_show_filter['highlight'], 'yes') == 0)
-	{
-      if ($isHighlight !== false)
-      {
-	    $image = 'highlight_on.png';
-		$alt_text = 'Klicken um alle Termine anzuzeigen';
-	  } else {
-	    $image = 'highlight_off.png';
-		$alt_text = 'Klicken um nur Highlights anzuzeigen';
-		if (strlen($newArgs) > 0) {
-		  $newArgs .= '&highlight=high';
-		} else {
-		  $newArgs = 'highlight=high';
-		}	
-	  }
-      echo '<a href="javascript:reload_evtermine();" class="callajax" data-vid="' . $event_vid . '" ';
-      echo 'data-count="' . $event_count . '" data-query="' . $newArgs . '" data-filter="' . $event_show_filter . '">' . "\n";
-	  echo '<img src="';
-      echo get_stylesheet_directory_uri();
-	  echo '/images/' . $image . '" title="'. $alt_text . '" class="evterm_hilight_image" />' . "\n";
-	  echo '</a>' . "\n";
-	}
-	
 	if (array_key_exists('noheadline', $event_show_filter) && strcmp($event_show_filter['noheadline'], 'yes') == 0)
 	{
 	  echo '</div>' . "\n";
