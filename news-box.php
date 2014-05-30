69ea7b244aade5ca578e9e8eabc93f0c01de7e90
Stefan Wagner <stefan.wagner@logima.de>
2014-05-30 16:06:08 +0200

    WEB-3 Format of Newslist

--- news-box.php
+++ news-box.php
@@ -11,8 +11,9 @@
 
     define("NumberOfPosts", 6);
     define("StdNewsCategoryName", 'news');
-    define("DefaultImageUrlNamePart",'/himg/defaultNewsImage'); //Should be 64x64px; if a image tries to add the first category to the image if 
+    define("DefaultImageUrlNamePart",'/images/NewsDefault'); //Should be 64x64px; if a image tries to add the first category to the image if 
     define("DefaultImageUrlExtension", '.jpg');
+    define("TemplateDirectory", get_template_directory_uri());
 
     /*-------------------------*/
     $args = array('numberposts' => NumberOfPosts, 'post_status'=>"publish",'post_type'=>"post",'orderby'=>"post_date");
@@ -27,26 +28,14 @@
     $postslist = get_posts($args);
 	function getImageUrl($post)
 	{
-        $defaultImage = DefaultImageUrlNamePart . DefaultImageUrlExtension;
         $attch = FALSE;
         if (is_object($post)) {
             $attch = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), array(64,64));     
     	    if ($attch == false) {
-    		    $attachments = get_posts(array('post_type' => 'attachment',
-											    'posts_per_page' => -1,
-											    'post_parent' => $post->ID));
-    		    if (count($attachments) > 0) {
-    		        $attch = wp_get_attachment_image_src($attachments[0]->ID, array(64,64));
-    		    }
+                $attch = getAttachementImageUrl($post);
     	    }
     	    if ($attch == false) {
-                $imageNameForCategory = DefaultImageUrlNamePart . '_' . get_the_category_name($post->ID) . DefaultImageUrlExtension;
-                if (file_exists($imageNameForCategory)) {
-                    return $imageNameForCategory;
-                } else {
-                    return $defaultImage;
-                }
-    		 
+                return getDefaultImageUrl($post);
     	    }
     	    else {
 			    return $attch[0];
@@ -55,6 +44,37 @@
            return $defaultImage; 
         }
 	}
+    function getAttachementImageUrl($post) 
+    {
+        $attch = false;
+    	$attachments = get_posts(array('post_type' => 'attachment',
+										'posts_per_page' => -1,
+										'post_parent' => $post->ID));
+    	if (count($attachments) > 0) {
+    		$attch = wp_get_attachment_image_src($attachments[0]->ID, array(64,64));
+    	}
+        return $attch;
+    }
+    function getDefaultImageUrl($post)
+    {
+        $defaultImage = TemplateDirectory . DefaultImageUrlNamePart . DefaultImageUrlExtension;
+        $categoryImagePath = getDefaultCategoryImagePath($post);
+        if (file_exists($categoryImagePath)) {
+            return getDefaultCategoryImageUrl($post);
+        } else {
+            return $defaultImage;
+        }
+    }
+    function getDefaultCategoryImageUrl($post)
+    {
+        return TemplateDirectory . getDefaultCategoryImagePath($post);
+    }
+    function getDefaultCategoryImagePath($post)
+    {
+        $categoryName = get_the_category_name($post->ID);
+        $categoryName = str_replace(' ', '_', $categoryName);
+        return DefaultImageUrlNamePart . '_' . $categoryName  . DefaultImageUrlExtension;
+    }
     function get_the_category_name($postId) {
         $cat = get_the_category($postId);
         return $cat[0]->cat_name;
@@ -76,26 +96,29 @@
      <ul class="media-list">
  <?php foreach ($postslist as $post) : setup_postdata($post); ?>
 	    <li class="media">
+            <h4 class="media-heading">
+                <a href="<?php the_permalink(); ?>" 
+                    title="<?php the_title(); ?>"><?php the_title(); ?></a> 
+                    <small>vom <?php the_date(); ?></small>
+            </h4>
             <a class="pull-left" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
                 <?php if (has_post_thumbnail()) : ?>
                    <?php the_post_thumbnail('preview64x64'); ?>
                  <?php else : ?>
                  <img class="media-object size-64x64" width="64" height="64" 
                     src="<?php echo getImageUrl($post); ?>" 
-                    alt="<?php the_title(); ?>" style="background-color:#808080">
+                    alt="<?php the_title(); ?>">
+                 <!-- Categorie Image URL: <?php echo getDefaultCategoryImageUrl($post) ?>-->
                  <?php endif; ?>
              </a>
             <div class="media-body">
-				<h4 class="media-heading">
-                    <a href="<?php the_permalink(); ?>" 
-                       title="<?php the_title(); ?>"><?php the_title(); ?></a> 
-                    <small>vom <?php the_date(); ?></small></h4>
+
 				<div class="news-content">
                     <?php the_excerpt();?>
-					<p><small>Von <?php the_author(); ?>  in <?php the_category(' &gt; '); ?></small> <br />
-                        <a class="btn btn-small" 
+					<p><a class="btn btn-small" 
 							href="<?php the_permalink(); ?>" 
 							title="<?php the_title(); ?>">Alles lesen &raquo;</a><br />
+                        <small>Von <?php the_author(); ?>  in <?php the_category(' &gt; '); ?></small>
                     </p>
 				</div>
             </div>
