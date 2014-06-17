0d328f2aad9bc7a0a092b31489c89df270adedff
Stefan Wagner <stefan.wagner@logima.de>
2014-06-18 00:54:00 +0200

    fix first contact

--- page-first-contact-antwort.php
+++ page-first-contact-antwort.php
@@ -9,17 +9,24 @@ $to = array( "kontakt@cvjm-gostenhof.de",
   "webmaster@cvjm-nuernberg.de"
 );
 $subject = "Anfrage im First Contact";
-$headers = 'From: '. $to . "\r\n" .
-  'Reply-To: webmaster@cvjm-nuernberg.de' . "\r\n";
 
 $check_text = '';
 $mail_text = '';
 $vals = array_filter($_POST);
 if (!empty($vals))
 {
+	/*
+	array(4) { ["first_contact_mail"]=> string(31) "stefan.wagner@cvjm-nuernberg.de" ["first_contact_name"]=> string(6) "Stefan" ["first_contact_age"]=> string(14) "So Ende/Anfang" ["first_contact_issue"]=> string(112) "#2 Test, Test, Test, 1, 2, 3 Geht das so oder nicht so? Äh, daß ist sö ähnlich. Ciao! Stefan" } 
+	*/
+	$from = str_replace(':', '', strip_tags($vals["first_contact_mail"]));
+	$headers = "From: <$from> \r\n" .
+	  "To: $to \r\n".
+	  "Sender: webmaster@cvjm-nuernberg.de\r\n" . 
+	  "X-FirstContactVersion: 10 \r\n" ;
+
   foreach ($vals as $key => $value) {
     $check_text .= $value;
-    $mail_text .= $key . ': '. $value . "\n";
+    $mail_text .= $key . ": \r\n\t". $value . "\r\n";
   }
 
   if (strlen($check_text) > 0) {
@@ -34,7 +41,7 @@ get_header(); ?>
 <div class="main_container">
   <H1>Vielen Dank f&uuml;r deine Nachricht</H!>
   <?php
-    if (strlen($check_text) > 0) {
+    if (strlen($check_text) == 0) {
       echo '<p>Leider hast du nichts eingegeben. Deshalb wurde auch keine Nachricht verschickt.</p>' . "\n";
     } else if ($sent == false) {
       echo '<p>Es ist ein Fehler beim Senden der Nachricht aufgetreten. Bitte versuche es sp&auml;ter noch einmal.';
